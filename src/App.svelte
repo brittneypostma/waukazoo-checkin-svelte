7b9e7947a19a61b71185e6d24736b4da06d13d5f
Brittney Postma <brittney.postma@hotmail.com>
2019-10-04 21:42:19 -0400

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<style>
+  .container {
+    display: grid;
+    grid-template-columns: 10fr 1fr;
+    height: 100%;
+    text-align: center;
+  }
+  .left-container {
+    display: grid;
+    align-content: space-around;
+    background-color: rgb(106, 106, 248);
+  }
+  .list {
+    width: 400px;
+  }
+</style>
+
+<div class="container">
+  <div class="left-container">
+    <div>
+      <h2>Visitor</h2>
+    </div>
+    <div>
+      <h2>Volunteer</h2>
+    </div>
+  </div>
+  <div class="list">
+    <h2>People in the building</h2>
+    <ul>
+      <li />
+    </ul>
+  </div>
+
+</div>
