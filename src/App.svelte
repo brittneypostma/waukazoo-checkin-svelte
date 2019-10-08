55d92afe4e544bfecc627f65bffa16a1ef925517
sballgirl11 <brittney.postma@hotmail.com>
2019-10-08 18:59:49 -0400

    switched to just volunteer component and removed others

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,51 @@
 <script>
-  import Buttons from "./components/Buttons.svelte";
-  import List from "./components/List.svelte";
+  import Volunteer from "./components/Volunteer.svelte";
 </script>
 
 <style>
-  .container {
+  .body {
     display: grid;
-    grid-template-columns: 10fr 1fr;
+    overflow: none;
     height: 100%;
-    text-align: center;
+    grid-template-rows: 1fr 1fr;
   }
-  .left-container {
+
+  header {
+    width: 100%;
+    color: #111;
     display: grid;
-    align-items: space-around;
-    background-color: rgb(106, 106, 248);
+    grid-template-columns: 1fr 1fr;
+    justify-content: space-between;
+    padding: 0 1em;
   }
-  .list {
-    width: 400px;
+
+  .school {
+    font-size: 2rem;
+  }
+
+  .logo {
+    justify-self: flex-end;
+  }
+
+  main {
+    background-color: #111;
   }
 </style>
 
-<div class="container">
-  <div class="left-container">
-    <Buttons />
-  </div>
-  <div class="list">
-    <h2>People in the building</h2>
-    <!-- {#each listData as person}
-      <List
-        name={person.name}
-        location={person.location}
-        isCompleted={person.isCompleted} />
-    {/each} -->
-  </div>
+<div class="body">
+  <header class="header">
+    <div class="school">
+      <h2>
+        Waukazoo Elementary
+        <br />
+        <span>Visitor Check In</span>
+      </h2>
+    </div>
+    <div class="logo">
+      <img src="./logo.png" alt="logo" style=" height: 200px; width: 200px;" />
+    </div>
+  </header>
+  <main>
+    <Volunteer />
+  </main>
 </div>
